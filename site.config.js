9a10ccec27d842659319a81996f9b2c580380994
kwonkangin <107761803+kwonkangin@users.noreply.github.com>
2024-04-03 03:11:40 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,29 +1,30 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "kwonkangin",
+    name: "morethanmin",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "Design",
-    bio: " ",
+    role: "frontend developer",
+    bio: "I develop everything using node.",
     email: "morethanmin.dev@gmail.com",
-    youtube: "morethanmin",
-    github: "morethanmin"
+    linkedin: "morethanmin",
+    github: "morethanmin",
+    instagram: "",
   },
   projects: [
     {
-      name: `112morethan-log`,
+      name: `morethan-log`,
       href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "kainspiration",
+    title: "morethan-log",
     description: "welcome to morethan-log!",
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2020, // If leave this empty, current year will be used.
+  since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
