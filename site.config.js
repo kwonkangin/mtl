761b2011f72ce479e0e4cbd747bf066972aacb49
kwonkangin <107761803+kwonkangin@users.noreply.github.com>
2024-04-04 00:15:28 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -3,9 +3,9 @@ const CONFIG = {
   profile: {
     name: "kwonkangin",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
+    role: "",
+    bio: "",
+    email: "",
     linkedin: "",
     github: "",
     instagram: "kwonkangin",
@@ -18,7 +18,7 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-   title: "kainspiration",
+   title: "blog | kainspiration",
     description: "welcome to kainspiration",
   },
 
