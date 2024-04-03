a0b5976e4d8e365ef0610fd5df727a9f894de5f1
kwonkangin <107761803+kwonkangin@users.noreply.github.com>
2024-04-04 01:26:50 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "kwonkangin",
-    image: "https://i.ibb.co/FmRKHfV/cats-jpg.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "https://i.ibb.co/z2pkWZ0/1312.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "",
     bio: "",
     email: "",
